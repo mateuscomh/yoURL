f3e45e980b93b36a89152a5e659e8460657e5d8d
Matheus Martins <3mhenrique@gmail.com>
2022-06-20 21:16:17 -0300

    Update sintax and header

--- postweb.sh
+++ postweb.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env sh
 
-#-----------HEADER----------------------------------------------------|
+#---------------------------------------------------------------------|
 #  Matheus Martins 3mhenrique@gmail.com
 #  https://github.com/mateuscomh
-#  Birth: 30/03/2021 v1.5  GPL3
+#  30/03/2021 v1.5  GPL3
 #  Encurtador de URL tinyurl e envio de arquivos para 0x0.st.
-#
-# Depende: curl, notify-send, xclip
+#  Depends: curl, notify-send, xclip
 #---------------------------------------------------------------------|
-
 
 case "$1" in
   -h | --help | '' ) echo "Encurtador de links e envio de arquivos e imagens para 0x0.st"
-  echo "Utilizar postweb.sh + path imagem/arquivo ou endereço" 
-  exit 0
-  ;;
-
+    echo "Utilizar postweb.sh + path imagem/arquivo ou endereço" 
+  exit 0 ;;
   -v | --version) echo "Versao 1.5"
-  exit 0
-  ;;
-
+  exit 0 ;;
   *) 
     if [ ! -f "$1" ] ; then
-      #TEXT=$(curl -sF"shorten=$1" https://0x0.st)
       TEXT=$(curl -s http://tinyurl.com/api-create.php?url="$1")
       if [ $? -eq "0" ] ; then
         echo "$TEXT"
@@ -31,10 +24,8 @@ case "$1" in
       else
         exit 1
       fi
-   fi
-      ;;
+   fi ;;
 esac
-
 TEXT=$(curl -sF "file"=@"$1" https://0x0.st | sed -e "s/<.*//")
 echo "$TEXT"
 notify-send -h int:value:40 "Upload realizado com sucesso"; xmessage -title "Send" -buttons "Close" -nearmouse "$TEXT"
